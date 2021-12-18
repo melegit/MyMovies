505cd482cdbe4fddfee009b4bc2e0c97456e979d
mele <mele@yahoo.es>
2021-12-18 02:24:32 +0100

    Configuración del adaptador
    - creación de layout item para el recycler
    - implementación del adapter
    - uso de Glide para cargar imágenes desde URL's
    - añadido evento de click para cada item del adapter, mostrando toast con el título del item.

--- app/src/main/java/mobi/mele/mymovies/MainActivity.kt
+++ app/src/main/java/mobi/mele/mymovies/MainActivity.kt
@@ -10,23 +10,34 @@ import mobi.mele.mymovies.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+
+    //96aed4cc9613c9d5dcf253359ce8f50a
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.recycler.adapter = MoviesAdapter(listOf(
-            Movie("Title 1", "url 1"),
-            Movie("Title 2", "url 2"),
-            Movie("Title 3", "url 3"),
-            Movie("Title 4", "url 4"),
-            Movie("Title 5", "url 5")
-        ))
+            Movie("Title 1", "https://loremflickr.com/320/240?lock=1"),
+            Movie("Title 2", "https://loremflickr.com/320/240?lock=2"),
+            Movie("Title 3", "https://loremflickr.com/320/240?lock=3"),
+            Movie("Title 4", "https://loremflickr.com/320/240?lock=4"),
+            Movie("Title 5", "https://loremflickr.com/320/240?lock=5"),
+            Movie("Title 6", "https://loremflickr.com/320/240?lock=6"),
+            Movie("Title 7", "https://loremflickr.com/320/240?lock=7"),
+            Movie("Title 8", "https://loremflickr.com/320/240?lock=8"),
+            Movie("Title 9", "https://loremflickr.com/320/240?lock=9"),
+            Movie("Title 10", "https://loremflickr.com/320/240?lock=10"),
+        ),
+        object : MovieClickedListener{
+            override fun onMovieClicked(movie: Movie) {
+                Toast.makeText(this@MainActivity,movie.title, Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 
     override fun onDestroy() {
         super.onDestroy()
         Log.d("MainActivity", "onDestroy")
     }
-
 }
--- app/src/main/java/mobi/mele/mymovies/MoviesAdapter.kt
+++ app/src/main/java/mobi/mele/mymovies/MoviesAdapter.kt
@@ -4,37 +4,54 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import mobi.mele.mymovies.databinding.ViewMovieItemBinding
 
-class MoviesAdapter(private val movies: List<Movie>) :
+interface MovieClickedListener {
+    fun onMovieClicked(movie: Movie)
+}
+
+class MoviesAdapter(private val movies: List<Movie>, private val movieClickedListener: MovieClickedListener) :
     RecyclerView.Adapter<MoviesAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 /*
-        Sin viewBinding
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.view_movie_item, parent, false)
-        return ViewHolder(view)*/
-/*
-        con viewBinding
-*/
-        val binding = ViewMovieItemBinding.inflate(LayoutInflater.from(parent.context),
-        parent,
-        false)
+        //Sin viewBinding
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.view_movie_item, parent, false)
+        return ViewHolder(view)
+        */
+
+        //con viewBinding
+
+        val binding = ViewMovieItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
 
         return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(movies[position])
+        val movie = movies[position]
+        holder.bind(movie)
+        holder.itemView.setOnClickListener { movieClickedListener.onMovieClicked(movie) }
     }
 
     override fun getItemCount(): Int {
         return movies.size
     }
 
-    class ViewHolder(binding: ViewMovieItemBinding) : RecyclerView.ViewHolder(binding.root){
-        fun bind(movie: Movie){
-
+    class ViewHolder(private val binding: ViewMovieItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(movie: Movie) {
+            binding.title.text = movie.title
+            Glide
+                .with(binding.root.context)
+                .load(movie.cover)
+                .into(binding.cover)
         }
     }
 }
